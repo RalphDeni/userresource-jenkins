047894a9b160765f5bcb54b5c222cdab4ca48350
Ralph <ralphdeni3@gmail.com>
2026-05-15 12:26:46 +0530

    updated img

--- userresource.py
+++ userresource.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///c:/Users/ulhas/userresource.db"
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///userresource.db"
 CORS(app)
 api=Api(app)
 db=SQLAlchemy(app)
